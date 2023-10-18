0943c6f6de3461ed8657d2ceac8a61277dd0408f
minhcang294 <147481478+minhcang294@users.noreply.github.com>
2023-10-18 07:27:36 +0700

    Add files via upload

---
+++ xuat mang 110123005.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+#include<stdlib.h>
+ void xuatMang (int* arr, int n)
+ {
+ 	int i;
+ 	    
+ 	    
+ 	    
+ 	printf(" Mang vua nhap la: ");
+ 	for(i=0; i<n; i++);
+ 	{
+ 	printf("&d", arr[i]);
+ 	}
+ 	printf("\n");
+}
+    int main()
+    {}
